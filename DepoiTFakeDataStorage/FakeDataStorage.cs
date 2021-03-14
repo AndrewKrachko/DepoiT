4e59e05ed160e07a00d297a5b36e6f1a8fbfe720
AndrewKrachko <AndrewKrachko@gmail.com>
2021-03-14 23:29:31 +0300

    FakeDataStorage implementation

--- DepoiTFakeDataStorage/FakeDataStorage.cs
+++ DepoiTFakeDataStorage/FakeDataStorage.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿using DepoiTItems;
+using DepoiTRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DepoiTFakeDataStorage
 {
-    public class FakeDataStorage
+    public class FakeDataStorage : IDataStorage
     {
+        private List<User> _users = new List<User>() {
+            new User() { Id=0, Name="admin", Password = "123", Email="admin@mail.com" },
+            new User() { Id=1, Name="user", Password = "321", Email="user@mail.com" }
+        };
+
+        public IUser GetUserByName(string name)
+        {
+            return _users.FirstOrDefault(u => u.Name == name);
+        }
     }
 }
