03a074409df4fe36e4bdb323f3e67f6586fff5ab
AndrewKrachko <AndrewKrachko@gmail.com>
2021-03-14 23:32:35 +0300

    Repository GetUserByName method implementation

---
+++ DepoiTRepository/IDataStorage.cs
@@ -0,0 +1,12 @@
+﻿using DepoiTItems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepoiTRepository
+{
+    public interface IDataStorage
+    {
+        IUser GetUserByName(string name);
+    }
+}
--- DepoiTRepository/Repository.cs
+++ DepoiTRepository/Repository.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using DepoiTItems;
+using System;
 
 namespace DepoiTRepository
 {
     public class Repository : IRepository
     {
+        private IDataStorage _dataStorge;
+
+        public bool GetUserByName(string name, out IUser user)
+        {
+            try
+            {
+                user = _dataStorge.GetUserByName(name);
+
+                if (user != null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
