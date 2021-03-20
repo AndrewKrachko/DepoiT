d11e882ebfbb3a750590faa7a255c2f1ff23696d
AndrewKrachko <AndrewKrachko@gmail.com>
2021-03-20 10:50:03 +0300

    Core implementation

---
+++ DepoiTCore/Core.cs
@@ -0,0 +1,42 @@
+﻿using DepoiTItems;
+using System;
+using System.Collections.Generic;
+
+namespace DepoiTCore
+{
+    public class Core : ICore
+    {
+        private readonly IRepository _repository;
+        private readonly ILogger _logger;
+
+        public Core(IRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public bool GetDepot(int id, IUser user, out IDepot depot)
+        {
+            if(_repository.GetDepot(id, user, out depot))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool GetDepots(IUser user, out IEnumerable<IDepot> depots)
+        {
+            if (_repository.GetDepots(user, out depots))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DepoiTCore/ICore.cs
@@ -0,0 +1,11 @@
+﻿using DepoiTItems;
+using System.Collections.Generic;
+
+namespace DepoiTCore
+{
+    public interface ICore
+    {
+        bool GetDepot(int id, IUser user, out IDepot depot);
+        bool GetDepots(IUser user, out IEnumerable<IDepot> depots);
+    }
+}
