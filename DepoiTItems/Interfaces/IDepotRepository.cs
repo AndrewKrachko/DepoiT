90e4fb870425f7494b367c21ef542bf0d9815b7a
AndrewKrachko <AndrewKrachko@gmail.com>
2021-03-27 18:35:03 +0300

    Minor refactoring

--- DepoiTItems/Interfaces/IDepotRepository.cs
+++ DepoiTItems/Interfaces/IDepotRepository.cs
@@ -4,7 +4,7 @@ namespace DepoiTItems
 {
     public interface IDepotRepository
     {
-        bool GetDepots(IEnumerable<int> id, out IEnumerable<IDepot> depot);
+        bool GetDepots(IEnumerable<int> id, out IEnumerable<IDepot> depots);
         bool GetDepotsByUser(int userId, out IEnumerable<IDepot> depots);
         bool SetDepot(IDepot depot, out IDepot createdDepot);
         bool UpdateDepot(IDepot depot, out IDepot updatedDepot);
