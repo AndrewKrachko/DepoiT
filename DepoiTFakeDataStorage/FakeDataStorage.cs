2cf8001893c10339c463bc2965bb0354623b019d
AndrewKrachko <AndrewKrachko@gmail.com>
2021-03-27 17:54:09 +0300

    Storage Item and fake data

--- DepoiTFakeDataStorage/FakeDataStorage.cs
+++ DepoiTFakeDataStorage/FakeDataStorage.cs
@@ -18,6 +18,8 @@ namespace DepoiTFakeDataStorage
 
         private List<Depot> _depots;
 
+        private List<Storage> _storages;
+
         public FakeDataStorage()
         {
             _tokenCharset = StringGenerator.StringGenerator.GetCharArrayWithinAsciiRange();
@@ -36,11 +38,26 @@ namespace DepoiTFakeDataStorage
                 new Address() { Id = 2, Street = "Odoevskogo", ObjectToken =@"""$L<}HHog!y6uR]6P|EN(ynT\", },
             };
 
+            _storages = new List<Storage>()
+            {
+                new Storage() { Id = 0, Name = "A", NameB = "4", NameC = "M1", ObjectToken = @"?""ihr(+zEvQ+A+9u1pjE2@%j1"},
+                new Storage() { Id = 1, Name = "Abrams", NameB = "", NameC = "M1", NameSplitter= " ", ObjectToken = @"aa~u8e)SnN9lo""f5-xPf*G@rz"},
+                new Storage() { Id = 2, Name = "M", NameB = "4", NameC = "Sherman", NameSplitter= " ", ObjectToken = @"D;&VR~|}X1F+%-2IWQ\!)@o$0"},
+                new Storage() { Id = 3, Name = "B-52", NameSplitter= " ", ObjectToken = @"%GT%(;B-yrJ$|<O;n8oA+(k>>"},
+                new Storage() { Id = 4, Name = "vfvb2wrrb", NameB = "", NameC = "LFKN Efcvnaso", NameSplitter= "---", ObjectToken = @"Rtbqr$K""FNWIyQu$|+9gEY6.d"},
+                new Storage() { Id = 5, Name = "ojrvk kMEMF", NameB = " lf,3mq89", NameC = "UHUIEfv98dsdvs", NameSplitter= "-", ObjectToken = @"9)!a&|[6eTE^;'o~""Vmvqtfx="},
+                new Storage() { Id = 6, Name = "\"My", NameB = "name is", NameC = "Vasia", NameSplitter= " ", ObjectToken = @"-|RVww_U.YzJ@@Q{34{PEb$Ih"},
+                new Storage() { Id = 7, Name = "Bh", NameB = "4", NameSplitter= "", ObjectToken = @"D$G6qDB'Dx~*_m\e)-LtT`6=z"},
+            };
+
             _depots = new List<Depot>
             {
-                new Depot() {Id = 0, Name = "Default depot", Adress = _addresses[0], Owner = _users[0], IsPublic = false, ObjectToken = @"&pDobMl2HeH(5N.bAXhieU_hp"},
-                new Depot() {Id = 1, Name = "User depot 1", Adress = _addresses[1], Owner = _users[1], IsPublic = false, ObjectToken=@"M)F;b'4v_xV$~%S.$Ml5/;O7j"},
-                new Depot() {Id = 2, Name = "User depot 2", Adress = _addresses[2], Owner = _users[1], IsPublic = false, ObjectToken=@"fB'juP2[~6DGue^40UUV_BT6\"},
+                new Depot() {Id = 0, Name = "Default depot", Adress = _addresses[0], Owner = _users[0], IsPublic = false, ObjectToken = @"&pDobMl2HeH(5N.bAXhieU_hp",
+                    Storages = new []{ _storages[0], _storages[1], _storages[2] } },
+                new Depot() {Id = 1, Name = "User depot 1", Adress = _addresses[1], Owner = _users[1], IsPublic = false, ObjectToken=@"M)F;b'4v_xV$~%S.$Ml5/;O7j",
+                    Storages = new []{ _storages[3], _storages[4], _storages[5] } },
+                new Depot() {Id = 2, Name = "User depot 2", Adress = _addresses[2], Owner = _users[1], IsPublic = false, ObjectToken=@"fB'juP2[~6DGue^40UUV_BT6\", 
+                    Storages = new []{ _storages[6], _storages[7] } },
             };
         }
 
---
+++ DepoiTItems/Items/Storage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepoiTItems
+{
+    public class Storage : IStorage
+    {
+        public string NameB { get; set; }
+        public string NameC { get; set; }
+        public string NameSplitter { get; set; }
+        public IEnumerable<IItem> Items { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ObjectToken { get; set; }
+    }
+}
