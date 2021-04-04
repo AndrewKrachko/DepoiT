8ae17c08fa9786e2a8c8d5630e076fbb4e5c5831
AndrewKrachko <AndrewKrachko@gmail.com>
2021-04-04 21:06:05 +0300

    EFCore DS

---
+++ DepoiTEFDataStorage/EFDataStorage.cs
@@ -0,0 +1,35 @@
+﻿using DepoiTItems;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DepoiTEFDataStorage
+{
+    public class EFDataStorage : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Depot> Depots { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Pattern> Patterns { get; set; }
+
+        public EFDataStorage()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new[]{
+                new User() { Id = 0, Name = "admin", Password = "123", UserToken = "87h4vhusd1", Email = "admin@mail.com", ObjectToken = @"a(qSa3Y/P{d.-iHDG~n/f.g/""" },
+                new User() { Id = 1, Name = "user", Password = "321", UserToken = "02vtr39sfd", Email = "user@mail.com", ObjectToken = @"~ex`+Elp.4I@)>#j8Fix'$j-2" }
+                });
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
